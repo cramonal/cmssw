2e66513b7ccc4a0329f679a2c7c5c57929cef156
yulunmiao <yulunmiao@gmail.com>
2025-09-19 17:16:04 +0200

    Debug on Configuration ESProducer

--- RecoLocalCalo/HGCalRecAlgos/plugins/HGCalTriggerConfigurationESProducer.cc
+++ RecoLocalCalo/HGCalRecAlgos/plugins/HGCalTriggerConfigurationESProducer.cc
@@ -10,7 +10,7 @@
 #include "FWCore/ParameterSet/interface/FileInPath.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 
-#include "CondFormats/HGCalObjects/interface/HGCALTriggerConfiguration.h"
+#include "CondFormats/HGCalObjects/interface/HGCalTriggerConfiguration.h"
 #include "CondFormats/HGCalObjects/interface/HGCalMappingModuleIndexerTrigger.h"
 #include "CondFormats/DataRecord/interface/HGCalElectronicsMappingRcd.h"
 #include "CondFormats/DataRecord/interface/HGCalModuleConfigurationRcd.h"  // depends on HGCalElectronicsMappingRcd
@@ -22,9 +22,9 @@
 /**
  * @short ESProducer to parse HGCAL electronics configuration from JSON file
  */
-class HGCALTriggerConfigurationESProducer : public edm::ESProducer, public edm::EventSetupRecordIntervalFinder {
+class HGCalTriggerConfigurationESProducer : public edm::ESProducer, public edm::EventSetupRecordIntervalFinder {
 public:
-  explicit HGCALTriggerConfigurationESProducer(const edm::ParameterSet& iConfig)
+  explicit HGCalTriggerConfigurationESProducer(const edm::ParameterSet& iConfig)
       :  //edm::ESProducer(iConfig),
         fedjson_(iConfig.getParameter<std::string>("fedjson")),
         modjson_(iConfig.getParameter<std::string>("modjson")) {
@@ -51,9 +51,9 @@ public:
     return (value_override >= 0 ? value_override : value);
   }
 
-  std::unique_ptr<HGCALTriggerConfiguration> produce(const HGCalModuleConfigurationRcd& iRecord) {
+  std::unique_ptr<HGCalTriggerConfiguration> produce(const HGCalModuleConfigurationRcd& iRecord) {
     auto const& moduleMap = iRecord.get(indexToken_);
-    edm::LogInfo("HGCALTriggerConfigurationESProducer")
+    edm::LogInfo("HGCalTriggerConfigurationESProducer")
         << "produce: fedjson_=" << fedjson_ << ",\n         modjson_=" << modjson_;
 
     // retrieve values from custom JSON format (see HGCalCalibrationESProducer)
@@ -71,14 +71,14 @@ public:
     // const std::vector<std::string> modkeys = {"headerMarker", "CalibrationSC"};
     const std::vector<std::string> modkeys = {"density", "dropLSB", "select", "stc_type", "eporttx_numen","calv","mux"};
     if (nfeds != fed_config_data.size())
-      edm::LogWarning("HGCALTriggerConfigurationESProducer")
+      edm::LogWarning("HGCalTriggerConfigurationESProducer")
           << "Total number of FEDs found in JSON file " << fedjson_ << " (" << fed_config_data.size()
           << ") does not match indexer (" << nfeds << ")";
 
     // loop over FEDs in indexer & fill configuration structs: FED > ECON-D > eRx
     // follow indexing by HGCalMappingModuleIndexer
-    // HGCALTriggerConfiguration = container class holding FED structs of ECON-D structs of eRx structs
-    std::unique_ptr<HGCALTriggerConfiguration> config_ = std::make_unique<HGCALTriggerConfiguration>();
+    // HGCalTriggerConfiguration = container class holding FED structs of ECON-D structs of eRx structs
+    std::unique_ptr<HGCalTriggerConfiguration> config_ = std::make_unique<HGCalTriggerConfiguration>();
     config_->feds.resize(moduleMap.maxFEDSize());
     for (std::size_t fedid = 0; fedid < moduleMap.maxFEDSize(); fedid++) {
       // sanity checks
@@ -88,14 +88,14 @@ public:
 
       if (moduleMap.fedReadoutSequences()[fedid].readoutTypes_.empty())            // check if FED exists (non-empty)
         continue;                                                                  // skip non-existent FED
-      if (fed_config_data[fedkey]['tdaqFlag'].size() != fed_config_data[fedkey]['neconts'].size()) // check if tdaqFlag and neconts have the same length(number of TDAQs)
+      if (fed_config_data[fedkey]["tdaqFlag"].size() != fed_config_data[fedkey]["neconts"].size()) // check if tdaqFlag and neconts have the same length(number of TDAQs)
         continue;
-      uint32_t nTDAQ = fed_config_data[fedkey]['tdaqFlag'].size();
+      uint32_t nTDAQ = uint32_t(fed_config_data[fedkey]["tdaqFlag"].size());
       uint32_t totalECONTs=0;
-      for (std::size_t itdaq=0;i<nTDAQ;itdaq++){
-        totalECONTs+=fed_config_data[fedkey]['neconts'][itdaq]
+      for (std::size_t itdaq=0;itdaq<nTDAQ;itdaq++){
+        totalECONTs+=uint32_t(fed_config_data[fedkey]["neconts"][itdaq]);
       }
-      if (moduleMap.getNumModules(fedid) != fed_config_data[fedkey]['econtSwapOffset'].size()
+      if (moduleMap.getNumModules(fedid) != fed_config_data[fedkey]["econtSwapOffset"].size()
         || moduleMap.getNumModules(fedid) != totalECONTs)             // check if length of sawp offsets, number of ECONTs in FED read from module locator, and number of econts summed mathces
         continue;
 
@@ -103,18 +103,18 @@ public:
       HGCalTriggerFedConfig fedConfig;
       
       // fill econtSwapOffset
-      fedConfig.econtSwapOffest.resize(moduleMap.getNumModules(fedid));
+      fedConfig.econtSwapOffset.resize(moduleMap.getNumModules(fedid));
       for (std::size_t iecont=0;iecont<moduleMap.getNumModules(fedid); iecont++){
-        fedConfig.econtSwapOffset[iecont] = fed_config_data[fedkey]['econtSwapOffset'][iecont]
+        fedConfig.econtSwapOffset[iecont] = int(fed_config_data[fedkey]["econtSwapOffset"][iecont]);
       }
       // fill TDAQ configurations
       fedConfig.tdaqs.resize(nTDAQ);
       totalECONTs = 0;
-      for (std::size_t itdaq=0;i<nTDAQ;itdaq++){
+      for (std::size_t itdaq=0;itdaq<nTDAQ;itdaq++){
         HGCalTDAQConfig tdaqConfig;
-        tdaqConfig.tdaqBlockHeaderMarker=std::stoi(fed_config_data[fedkey]['tdaqHeaderMarker'], nullptr, 16);
-        tdaqConfig.tdaqFlag=fed_config_data[fedkey]['tdaqFlag'][itdaq];
-        uint32_t nECONT = fed_config_data[fedkey]['neconts'];
+        tdaqConfig.tdaqBlockHeaderMarker=std::stoi(std::string(fed_config_data[fedkey]["tdaqHeaderMarker"]), nullptr, 16);
+        tdaqConfig.tdaqFlag=fed_config_data[fedkey]["tdaqFlag"][itdaq];
+        uint32_t nECONT = uint32_t(fed_config_data[fedkey]["neconts"]);
 
         for (const auto& [typecode, ids] : moduleMap.typecodeMap()) {
           auto [fedid_, imod] = ids;
@@ -124,37 +124,38 @@ public:
           hgcal::check_keys(
               mod_config_data, modkey, modkeys, modjson_);  // check required keys are in the JSON, warn otherwise
           //sanity check
-          nTC_calv = mod_config_data[modkey]["calv"].size();
-          nTC_mux = mod_config_data[modkey]["tcMux"].size();
-          nTC = moduleMap.getNumChannels(typecode)
+          size_t nTC_calv = mod_config_data[modkey]["calv"].size();
+          size_t nTC_mux = mod_config_data[modkey]["tcMux"].size();
+          size_t nTC = moduleMap.getNumChannels(typecode);
           if(nTC != nTC_mux || nTC != nTC_calv){
             continue;
           }
           HGCalECONTConfig econtConfig;
-          econtConfig.density = mod_config_data[modkey]["density"];
-          econtConfig.dropLSB = mod_config_data[modkey]["dropLSB"];
-          econtConfig.select = mod_config_data[modkey]["select"];
-          econtConfig.stcType = mod_config_data[modkey]["stcType"];
-          econtConfig.eportTxNumen = mod_config_data[modkey]["eportTxNumen"];
+          econtConfig.density = uint8_t(mod_config_data[modkey]["density"]);
+          econtConfig.dropLSB = uint8_t(mod_config_data[modkey]["dropLSB"]);
+          econtConfig.select = uint8_t(mod_config_data[modkey]["select"]);
+          econtConfig.stcType = uint8_t(mod_config_data[modkey]["stcType"]);
+          econtConfig.eportTxNumen = uint8_t(mod_config_data[modkey]["eportTxNumen"]);
 
           econtConfig.calv.resize(nTC);
           econtConfig.tcMux.resize(nTC);
-          econtConfig.offset.resize();
+          econtConfig.offset.resize(nTC);
           for(std::size_t iTC;iTC<nTC;iTC++){
-            econtConfig.calv[iTC] = mod_config_data[modkey]['calv'][iTC];
-            econtConfig.tcMux[iTC] = mod_config_data[modkey]['tcMux'][iTC];
+            econtConfig.calv[iTC] = mod_config_data[modkey]["calv"][iTC];
+            econtConfig.tcMux[iTC] = mod_config_data[modkey]["tcMux"][iTC];
             econtConfig.offset[iTC] = calculateCellOffset(); //TODO: change this when we know how to calcualte
           }
           // Caculate module number in the TDAQ
-          iecont = imod - totalECONTs;
+          uint32_t iecont = imod - totalECONTs;
           tdaqConfig.econts[iecont] = econtConfig;
         }
         fedConfig.tdaqs[itdaq]=tdaqConfig;
-        totalECONTs += fed_config_data[fedkey]['neconts'][itdaq];
+        totalECONTs += uint32_t(fed_config_data[fedkey]["neconts"][itdaq]);
       }
-      config_->feds[fedid] = fed;
+      config_->feds[fedid] = fedConfig;
     }
     std::cout<<config_<<std::endl;
+    LogDebug("HGCalTriggerConfigurationESProducer") << *config_;
     return config_;
   }  // end of produce()
 
@@ -173,4 +174,4 @@ private:
   const std::string modjson_;       // JSON file
 };
 
-DEFINE_FWK_EVENTSETUP_SOURCE(HGCALTriggerConfigurationESProducer);
+DEFINE_FWK_EVENTSETUP_SOURCE(HGCalTriggerConfigurationESProducer);
